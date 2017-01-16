55db0d79cf7e533809a2fc49a69f303ee630088c
steven-borthwick <stevenborthwick@hotmail.com>
2017-01-16 15:32:33 +0000

    Postcode lookup

--- app/routes.js
+++ app/routes.js
@@ -986,6 +986,125 @@ router.get('/contingency_test3/print_v2', function (req, res) {
 
 });
 
+// contigency_test_postcode
+
+// contingency_postcode
+
+
+
+router.get('/contingency_postcode/results_confirm2', function (req, res) {
+
+  var search = req.query.search ? req.query.search.toLowerCase() : '';
+
+  res.render('contingency_postcode/results_confirm2', {
+    'search' : req.query.search,
+    'excluded': search === 'aa678910c',
+    'included': search === 'bb123456c',
+    'name': search === 'cd654321c',
+    'address': search === 'yy109876c',
+    'special': search === 'gg123456c'
+  });
+});
+
+router.get('/contingency_postcode/results_confirm3', function (req, res) {
+
+  var search = req.query.search ? req.query.search.toLowerCase() : '';
+
+  res.render('contingency_postcode/results_confirm3', {
+    'search' : req.query.search,
+    'excluded': search === 'aa678910c',
+    'included': search === 'bb123456c',
+    'name': search === 'cd654321c',
+    'address': search === 'yy109876c',
+    'special': search === 'gg123456c'
+  });
+});
+
+router.get('/contingency_postcode/ole_details', function (req, res) {
+
+  var search = req.query.search ? req.query.search.toLowerCase() : '';
+
+  res.render('contingency_postcode/ole_details', {
+    'search' : req.query.search,
+    'excluded': search === 'aa678910c',
+    'included': search === 'bb123456c',
+    'name': search === 'cd654321c',
+    'address': search === 'yy109876c',
+    'special': search === 'gg123456c'
+  });
+});
+
+router.get('/contingency_postcode/issue_address_confirm', function (req, res) {
+
+  // get the answer from the query string (eg. ?address_same=No)
+  var details_same = req.query.details_same;
+
+  if (details_same == "Yes"){
+
+    // if address_same is any other value (or is missing) render the page requested
+    res.render('contingency_postcode/issue_address_confirm');
+
+
+  } else {
+
+          // redirect to the relevant page
+    res.redirect('/contingency_postcode/issue_cant_details');
+
+  }
+
+});
+
+router.get('/contingency_postcode/issue_address_exclusion', function (req, res) {
+
+  // get the answer from the query string (eg. ?address_same=No)
+  var details_same2 = req.query.details_same2;
+
+  if (details_same2 == "Yes"){
+
+    // if address_same is any other value (or is missing) render the page requested
+    res.render('contingency_postcode/issue_address_exclusion');
+
+
+  } else {
+
+          // redirect to the relevant page
+    res.redirect('/contingency_postcode/issue_cant_details');
+
+  }
+
+});
+
+
+
+router.get('/contingency_postcode/print', function (req, res) {
+
+  // get the answer from the query string (eg. ?address_same=No)
+  var address_same = req.query.address_same;
+
+  if (address_same == "Yes"){
+
+    // if address_same is any other value (or is missing) render the page requested
+    res.render('contingency_postcode/print');
+
+
+  } else {
+
+          // redirect to the relevant page
+    res.redirect('/contingency_postcode/issue_cant');
+
+  }
+
+});
+
+router.get('/contingency_postcode/print_v2', function (req, res) {
+
+  var NInumber = req.query.NInumber;
+
+  res.render('contingency_postcode/print_v2', { 'NInumber' : NInumber  });
+
+});
+
+
 // contingency_test4
 
 
